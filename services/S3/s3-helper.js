0a1e36c2d5bb6ed1b35b1edb59e6ca1c935469f3
eladdavid5 <elad.david5@gmail.com>
2023-05-01 23:16:03 +0300

    s3 helper file

--- services/S3/s3-helper.js
+++ services/S3/s3-helper.js
@@ -42,3 +42,45 @@ class ImageService {
 }
 
 module.exports = ImageService
+
+// const S3Service = require('./s3')
+
+// class ImageService {
+//   constructor() {
+//     this.s3Service = new S3Service()
+//     this.Bucket = process.env.BUCKET_NAME
+//     this.Key = process.env.OBJECT_KEY
+//   }
+
+//   async uploadImage(imageURL) {
+//     try {
+//       // Download the image data from the provided URL
+//       const imageBuffer = await this.s3Service.downloadImage(imageURL)
+
+//       // Upload the image data to the S3 bucket
+//       const result = await this.s3Service.uploadImage(this.bucketName, this.objectKey, imageBuffer)
+
+//       // Return the URL of the uploaded image
+//       return result.Location
+//     } catch (error) {
+//       console.error(`Error uploading image: ${error}`)
+//       throw error
+//     }
+//   }
+
+//   async getImage() {
+//     try {
+//       // Get the image data from the S3 bucket
+//       const imageData = await this.s3Service.getImage(this.bucketName, this.objectKey)
+
+//       // Return the image data
+//       return imageData.Body
+//     } catch (error) {
+//       console.error(`Error retrieving image: ${error}`)
+//       throw error
+//     }
+//   }
+// }
+
+// module.exports = ImageService
+
